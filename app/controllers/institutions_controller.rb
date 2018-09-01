435046f080a5872209b95a8fe5d914971b42e7e1
Ricardo Sousa <ricardocads@gmail.com>
2018-09-01 16:44:22 -0300

    story/004-creates-frontend

--- app/controllers/institutions_controller.rb
+++ app/controllers/institutions_controller.rb
@@ -28,7 +28,7 @@ class InstitutionsController < ApplicationController
 
     respond_to do |format|
       if @institution.save
-        format.html { redirect_to @institution, notice: 'Institution was successfully created.' }
+        format.html { redirect_to action: "index", notice: 'Institution was successfully created.' }
         format.json { render :show, status: :created, location: @institution }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class InstitutionsController < ApplicationController
   def update
     respond_to do |format|
       if @institution.update(institution_params)
-        format.html { redirect_to @institution, notice: 'Institution was successfully updated.' }
+        format.html { redirect_to action: "index", notice: 'Institution was successfully updated.' }
         format.json { render :show, status: :ok, location: @institution }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class InstitutionsController < ApplicationController
   def destroy
     @institution.destroy
     respond_to do |format|
-      format.html { redirect_to institutions_url, notice: 'Institution was successfully destroyed.' }
+      format.html { redirect_to action: "index", notice: 'Institution was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
