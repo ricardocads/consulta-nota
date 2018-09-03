c8cb7e44d8a2836015c33c04e7e6a8a126578c7f
Ricardo Sousa <ricardocads@gmail.com>
2018-09-02 22:19:37 -0300

    support/004-orders-rows-by-grade

--- app/assets/javascripts/layouts/default.js
+++ app/assets/javascripts/layouts/default.js
@@ -1,5 +1,6 @@
 $(document).ready( function () {
     $('#table').DataTable({
+      "order": [[ 1, "desc" ]],
       "language": {
           "sEmptyTable": "Nenhum registro encontrado",
               "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
